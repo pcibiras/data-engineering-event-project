aa8143c7b1a953617b206a1d878124b7c45b2dd0
Povilas Cibiras <p.cibiras@gmail.com>
2024-08-11 11:06:06 +0300

    initial commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_network" "kafka_network" {
+  name = "kafka-network"
+}
