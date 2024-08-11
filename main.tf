44b5a550b38ceb9dfaf7d2ebf6d19c4b3c3be2e7
Povilas Cibiras <p.cibiras@gmail.com>
2024-08-11 13:51:56 +0300

    add nats image, container definition

--- main.tf
+++ main.tf
@@ -14,3 +14,38 @@ provider "docker" {
 resource "docker_network" "kafka_network" {
   name = "kafka-network"
 }
+
+# resource "docker_container" "zookeeper" {
+#   image = "confluentinc/cp-zookeeper:latest"
+#   name  = "zookeeper"
+
+#   networks_advanced {
+#     name = docker_network.kafka_network.name
+#   }
+
+#   env = [
+#     "ZOOKEEPER_CLIENT_PORT = 2181"
+#   ]
+# }
+
+# resource "docker_image" "zookeeper" {
+#   name = "confluentinc/cp-zookeeper:latest"
+# }
+
+# resource "docker_image" "zookeeper" {
+#   name = "bitnami/zookeeper:latest"
+# }   
+
+resource "docker_image" "nats" {
+  name = "nats:latest"
+}
+
+resource "docker_container" "nats" {
+  image = docker_image.nats.name
+  name  = "nats"
+
+  ports {
+    internal = 4222
+    external = 4222
+  }
+}
