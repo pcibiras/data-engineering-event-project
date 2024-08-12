7c75c607bc9fadec1bbfd69823055b43e070a76e
Povilas Cibiras <p.cibiras@gmail.com>
2024-08-12 23:52:58 +0300

    delete postgres container and image

--- main.tf
+++ main.tf
@@ -11,30 +11,9 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
-resource "docker_network" "kafka_network" {
-  name = "kafka-network"
+resource "docker_network" "solution-network" {
+  name = "solution-network"
 }
-
-# resource "docker_container" "zookeeper" {
-#   image = "confluentinc/cp-zookeeper:latest"
-#   name  = "zookeeper"
-
-#   networks_advanced {
-#     name = docker_network.kafka_network.name
-#   }
-
-#   env = [
-#     "ZOOKEEPER_CLIENT_PORT = 2181"
-#   ]
-# }
-
-# resource "docker_image" "zookeeper" {
-#   name = "confluentinc/cp-zookeeper:latest"
-# }
-
-# resource "docker_image" "zookeeper" {
-#   name = "bitnami/zookeeper:latest"
-# }   
 
 resource "docker_image" "nats" {
   name = "nats:latest"
@@ -43,34 +22,12 @@ resource "docker_image" "nats" {
 resource "docker_container" "nats" {
   image = docker_image.nats.name
   name  = "nats"
+  networks_advanced {
+  name = docker_network.solution-network.name
+  }
 
   ports {
     internal = 4222
     external = 4222
   }
-}
-
-resource "docker_image" "postgres-14-image" {
-  name = "postgres:14.0"
-}
-
-resource "docker_container" "postgres-container" {
-  image = docker_image.postgres-14-image.name  # Use the PostgreSQL image version you prefer
-  name  = "postgres_db"
-  networks_advanced {
-    name = docker_network.kafka_network.name
-  }
-    ports {
-    internal = 5432
-    external = 5432
-  }
-    volumes {
-    host_path = "/data"
-    container_path = "/var/lib/postgresql/data"
-  }
-  env [
-    "POSTGRES_USER = {var.postgres_user}",
-    "POSTGRES_PASSWORD=${var.postgres_password}",
-    "POSTGRES_DB=${var.postgres_db}" 
-  ]
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,11 @@
-variable "postgres_user" {
-    description = "The PostgreSQL username"
-}
+# variable "postgres_user" {
+#     description = "The PostgreSQL username"
+# }
 
-variable "postgres_password" {
-    description = "The PostgreSQL password"
-}
+# variable "postgres_password" {
+#     description = "The PostgreSQL password"
+# }
+
+# variable "postgres_db" {
+#   description = "The PostgreSQL database name"
+# }
